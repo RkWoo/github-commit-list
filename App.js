28e88cc6c03d7e354dd29773aed748f35f67dd48
Richard Woo <rkwoo.sf@gmail.com>
2021-01-23 21:04:04 -0800

    UI/UX improvement — updated layout of the text input area

--- App.js
+++ App.js
@@ -22,24 +22,46 @@ const App: () => React$Node = () => {
 
   return (
     <View style={{flex:1, marginHorizontal:10}}>
-      <TextInput
-        style={styles.repoInput}
-        onChangeText={(text) => setRepoOwnerText(text)}
-        placeholder='repository owner'
-      >
-        {repoOwnerText}
-      </TextInput>
-      <TextInput
-        style={styles.repoInput}
-        onChangeText={(text) => setRepoNameText(text)}
-        placeholder='repository name'
-      >
-       {repoNameText}
-      </TextInput>
-      <Button
-        title='Get Commits'
-        disabled={isWaiting || repoNameText.length==0 || repoOwnerText==0}
-        onPress={() => onPressGetCommits()} />
+      <Text style={{fontSize:16, fontWeight:'bold', marginTop:10}}>
+        Select a GitHub repository
+       </Text>
+      <Text>
+        (https://github.com/
+        <Text style={styles.boldText}>owner</Text>
+        /
+        <Text style={styles.boldText}>name</Text>
+        )
+      </Text>
+      <View style={{flexDirection:'row', marginTop:4}}>
+        <View style={{flex:1, marginRight:10}}>
+          <Text style={styles.boldText}>owner</Text>
+          <TextInput
+            style={styles.repoInput}
+            onChangeText={(text) => setRepoOwnerText(text)}
+            onSubmitEditing={() => Keyboard.dismiss()}
+            placeholder='repository owner'
+          >
+            {repoOwnerText}
+          </TextInput>
+        </View>
+        <View style={{flex:1, marginLeft:10}}>
+          <Text style={styles.boldText}>name</Text>
+          <TextInput
+            style={styles.repoInput}
+            onChangeText={(text) => setRepoNameText(text)}
+            onSubmitEditing={() => Keyboard.dismiss()}
+            placeholder='repository name'
+          >
+            {repoNameText}
+          </TextInput>
+        </View>
+      </View>
+      <View style={{marginVertical:10}}>
+        <Button
+          title='Get Commits'
+          disabled={isWaiting || repoNameText.length==0 || repoOwnerText==0}
+          onPress={() => onPressGetCommits()} />
+      </View>
       { isWaiting &&
         <ActivityIndicator
           size='large'
@@ -105,8 +127,8 @@ export const getCommits = (repoOwner, repoName, maxCount) => {
 const styles = StyleSheet.create({
   repoInput: {
     borderWidth: 1,
-    borderColor: 'black',
-    marginVertical: 10,
+    borderColor: 'lightgray',
+    padding: 4,
   },
   commitItem: {
     flex: 1,
